65c48931e786cecb717aebf15a2736d5ce793a5b
JP Whitaker <jpw@hawaii.edu>
2023-12-17 16:03:37 -1000

    fish animation

---
+++ src/app/Experience.jsx
@@ -0,0 +1,9 @@
+import { Fish12 } from "./components/Fish12"
+export default function Experience(){
+  return (
+    <>
+    <ambientLight intensity={5}/>
+    <Fish12 scale={3.5}/>
+    </>
+  )
+}
